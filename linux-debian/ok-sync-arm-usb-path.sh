ce613e229d9ff2153a62615e80aa8018164750b1
Shawn <shawn.bourgeois2@gmail.com>
2016-11-02 17:47:17 -0400

    Create ok-sync-arm-usb-path.sh

---
+++ linux-debian/ok-sync-arm-usb-path.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+echo "initializing Okcash First time Sync"
+
+date
+
+OKUSER=$USER
+HOMEDIR="/home/$OKUSER"
+OKCASHPATH="/media/pi/youUSB"
+OKDEFAULT="$HOMEDIR/.okcash"
+
+echo "User: $OKUSER"
+echo "User home dir: $HOMEDIR"
+echo "User Okcash path: $OKCASHPATH"
+
+sudo apt-get install unzip pwgen -y
+
+# Create Client Directory
+if [ ! -e "$OKCASHPATH" ]
+then
+    mkdir $OKCASHPATH
+fi
+
+#make sure old blockchain zip or not fully downloaded data gets erased to prevent errors
+rm $OKCASHPATH/ok-blockchain.zip
+
+# Download OK Blockchain
+cd $OKCASHPATH
+wget https://github.com/okcashpro/ok-blockchain/releases/download/latest/ok-blockchain.zip
+cd $OKCASHPATH
+unzip ok-blockchain.zip
+
+# Create configuration File
+touch $OKCASHPATH/okcash.conf
+rpcu=$(pwgen -ncsB 35 1)
+rpcp=$(pwgen -ncsB 75 1)
+echo "rpcuser=$rpcu
+rpcpassword=$rpcp
+daemon=1" > "$OKCASHPATH"/okcash.conf
+
+#create okcash.conf and write datadir directory
+touch $OKDEFAULT/okcash.conf
+echo datadir=$OKCASHPATH > $OKDEFAULT/okcash.conf
+
+# Delete the downloaded blockchain zip file // free space from device
+rm $OKCASHPATH/ok-blockchain.zip
+
+# end Client
+
+echo "completed Okcash first time instant sync for:"
+uname -o
+echo "enjoy your OK experience"
+exit 0
