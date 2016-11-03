d441ab8bdde8e8daf262fc390b87c22b6dabde71
oktoshi <devteam@okcash.co>
2016-11-03 17:19:25 -0600

    structure and pass gen updt

--- linux/ok-sync-arm64.sh
+++ linux/ok-sync-arm64.sh
@@ -31,8 +31,8 @@ unzip ok-blockchain-arm64.zip
 
 # Create configuration File
 touch $OKCASHPATH/okcash.conf
-rpcu=$(pwgen -ncsB 35 1)
-rpcp=$(pwgen -ncsB 75 1)
+rpcu=$(pwgen -ncsB 20 1)
+rpcp=$(pwgen -ncsB 20 1)
 echo "rpcuser=$rpcu
 rpcpassword=$rpcp
 daemon=1" > "$OKCASHPATH"/okcash.conf
