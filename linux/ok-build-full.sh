1036dfca191c4f9e98b1c72c9c928c214128bcda
Shawn <shawn.bourgeois2@gmail.com>
2016-11-12 17:39:12 -0500

    added rpcu and rpcp
    Added the lines to create the okcash.conf file and create the rpcu and rpcp so that we don't have to do " ./okcashd " to get them and have to copy them in the okcash.conf. Makes the users save a step and stupid proof about how to use text editors.

--- linux/ok-build-full.sh
+++ linux/ok-build-full.sh
@@ -54,6 +54,13 @@ cp okcash $OKAPPSPATH
 
 sudo cp okcash /usr/local/bin
 
+touch $OKCASHPATH/okcash.conf
+rpcu=$(pwgen -ncsB 20 1)
+rpcp=$(pwgen -ncsB 20 1)
+echo "rpcuser=$rpcu
+rpcpassword=$rpcp
+daemon=1" > "$OKCASHPATH"/okcash.conf
+
 # end Client
 
 echo "Done compiling and installing OK from source: Okcash GUI and Daemon - okcash + okcashd - on its latest version for:"
