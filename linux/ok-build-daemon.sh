9d7c9c63a25ec0b33f708e49cbaf081cbfe7339f
oktoshi <devteam@okcash.co>
2019-01-19 20:28:05 -0600

    updt fix

--- linux/ok-build-daemon.sh
+++ linux/ok-build-daemon.sh
@@ -17,7 +17,7 @@ sudo add-apt-repository -y ppa:bitcoin/bitcoin
 
 sudo apt-get update
 
-sudo apt-get install -y libdb4.8-dev libdb4.8++-dev -y
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 
 if [ ! -e "$OKAPPSPATH" ]
 then
