7479a842f6a9d8ae4ed1e429e0ae9c056010dcd2
oktoshi <9932412+oktoshi@users.noreply.github.com>
2019-04-26 13:46:46 -0500

    u18

---
+++ linux/ok-build-full-u18.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+echo "Compiling Okcash GUI and Daemon - okcash + okcashd - from source on its latest version"
+
+date
+
+OKUSER=$USER
+HOMEDIR="/home/$OKUSER"
+OKAPPSPATH="$HOMEDIR/ok_apps"
+
+echo "User: $OKUSER"
+echo "User home dir: $HOMEDIR"
+echo "Okcash apps path: $OKAPPSPATH"
+
+sudo apt-get install git build-essential libssl-dev libssl1.0-dev libboost-all-dev libqrencode-dev libminiupnpc-dev -y
+
+sudo apt-get install qtcreator qt5-default qttools5-dev-tools  libqt5webkit5-dev -y
+
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+
+sudo apt-get update
+
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+
+if [ ! -e "$OKAPPSPATH" ]
+then
+        mkdir $OKAPPSPATH
+fi
+
+cd
+
+git clone https://github.com/okcashpro/okcash
+
+cd okcash
+
+cd src
+
+make -f makefile.unix USE_UPNP=-
+
+strip okcashd
+
+sudo cp okcashd /usr/local/bin
+
+cp okcashd $OKAPPSPATH
+
+cd ..
+
+qmake
+
+make
+
+strip okcash
+
+cp okcash $OKAPPSPATH
+
+sudo cp okcash /usr/local/bin
+
+# end Client
+
+echo "Done compiling and installing OK from source: Okcash GUI and Daemon - okcash + okcashd - on its latest version for:"
+uname -a
+echo "enjoy your OK experience"
+exit 0
