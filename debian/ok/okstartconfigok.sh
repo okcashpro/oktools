2cb2803b3b2aa906852b159c8dfdd88c82b40990
oktoshi <9932412+oktoshi@users.noreply.github.com>
2020-02-18 19:41:23 -0600

    update config data

--- debian/ok/okstartconfigok.sh
+++ debian/ok/okstartconfigok.sh
@@ -26,7 +26,15 @@ rpcu=$(pwgen -ncsB 20 1)
 rpcp=$(pwgen -ncsB 20 1)
 echo "rpcuser=$rpcu
 rpcpassword=$rpcp
-daemon=1" > "$OKCASHPATH"/okcash.conf
+#rpcport=6969
+#port=6970
+daemon=1
+#staking=0
+#server=1
+#listen=1
+#txindex=1
+#maxconnections=1024
+#timeout=15000" > "$OKCASHPATH"/okcash.conf
 
 # end Client
 echo "okcash.conf file created with random rpcuser and rpcpassword."
